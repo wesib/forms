aac9bae04ca51a1041da36fbae85e15bbbc56381
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-05-21 14:06:11 +0700

    Move forms support here from `@wesib/generic`

---
+++ README.md
@@ -0,0 +1,17 @@
+Wesib: Forms
+============
+
+[![NPM][npm-image]][npm-url]
+[![Build Status][build-status-img]][build-status-link]
+[![GitHub Project][github-image]][github-url]
+[![API Documentation][api-docs-image]][api-docs-url]
+
+[Wesib]: https://github.com/wesib/wesib
+[npm-image]: https://img.shields.io/npm/v/@wesib/forms.svg?logo=npm
+[npm-url]: https://www.npmjs.com/package/@wesib/forms
+[build-status-img]: https://github.com/wesib/forms/workflows/Build/badge.svg
+[build-status-link]: https://github.com/wesib/forms/actions?query=workflow%3ABuild
+[github-image]: https://img.shields.io/static/v1?logo=github&label=GitHub&message=project&color=informational
+[github-url]: https://github.com/wesib/forms
+[api-docs-image]: https://img.shields.io/static/v1?logo=typescript&label=API&message=docs&color=informational
+[api-docs-url]: https://wesib.github.io/forms/ 
---
+++ jest.config.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  preset: 'ts-jest',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/spec/**/*.ts',
+    '!src/**/*.spec.ts',
+    '!src/**/index.ts',
+    '!src/**/main.ts',
+    '!**/node_modules/**',
+  ],
+  coverageDirectory: 'target/coverage',
+  coverageThreshold: {
+    global: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
+  },
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        suiteName: '@wesib/forms',
+        outputDirectory: './target/test-results',
+        classNameTemplate: '{classname}: {title}',
+        titleTemplate: '{classname}: {title}',
+        ancestorSeparator: ' › ',
+        usePathForSuiteName: 'true',
+      },
+    ],
+  ],
+  restoreMocks: true,
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.spec.json',
+    },
+  },
+};
